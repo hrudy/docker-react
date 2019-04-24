5c812acb661b8a6696b297dd048be6b3321ad7d1
David J. Ruedeman <djr@ondesign.com>
2019-04-24 08:54:02 -0400

    changed app text for dev

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Turn me on hiney!
+            Turn me on developers !
           </p>
           <a
             className="App-link"
